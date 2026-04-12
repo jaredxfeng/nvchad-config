99052be8b5930ca18d16046ff8562a4d9ab83ef4
jaredxfeng <jared.feng@energytoolbase.com>
2026-04-11 21:29:49 -0600

    fix: load brain battery display

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,11 @@
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = { "jaredxfeng/brain-battery.nvim" },  -- ← this ensures correct load order
   opts = function(_, opts)
     opts.sections = opts.sections or {}
     opts.sections.lualine_x = vim.list_extend(
       opts.sections.lualine_x or {},
-      { require("brain-battery") }
+      { require("brain-battery").lualine_component() }  -- ← this was the missing piece
     )
     return opts
   end,

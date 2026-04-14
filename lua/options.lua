4076dc8d6095f822a8a506b6aebdc45e64b26928
jaredxfeng <jared.feng@energytoolbase.com>
2026-04-14 08:43:29 -0600

    fix: add number gutter; turn on rel numbers

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,8 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "auto"
+vim.opt.numberwidth = 4

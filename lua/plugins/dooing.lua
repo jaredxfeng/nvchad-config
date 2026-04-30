18b47546d559328b5a4bc8253092653e22efe46a
jaredxfeng <jared.feng@energytoolbase.com>
2026-04-29 19:17:07 -0600

    feat!: replace octo with github-pr-reviewer; add dooing (todo list)

---
+++ lua/plugins/dooing.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "atiladefreitas/dooing",
+    config = function()
+        require("dooing").setup({
+            -- your custom config here (optional)
+        })
+    end,
+    event = "VeryLazy",
+  },
+}
---
+++ lua/plugins/pr.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "otavioschwanck/github-pr-reviewer.nvim",
+    opts = {
+      -- options here
+    },
+    keys = {
+      { "<leader>p", "<cmd>PRReviewMenu<cr>",    desc = "PR Review Menu" },
+      { "<leader>p", ":<C-u>'<,'>PRSuggestChange<CR>", desc = "Suggest change", mode = "v" }
+    }
+  }
+}

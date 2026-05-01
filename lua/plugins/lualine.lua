ff199658f16a7df03267fc8b152571c9ddb1df97
jaredxfeng <jared.feng@energytoolbase.com>
2026-05-01 16:48:06 -0600

    feat: update lubline sec b to show arrowed files

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,9 +4,18 @@ return {
   event = "VeryLazy",
   opts = function(_, opts)
     opts.sections = opts.sections or {}
+    opts.sections.lualine_b = {
+        "branch",
+        {
+          function()
+            local statusline = require("arrow.statusline")
+            return statusline.text_for_statusline_with_icons()
+          end,
+        },
+    }
     opts.sections.lualine_x = vim.list_extend(
       opts.sections.lualine_x or {},
-      { require("brain-battery").lualine_component() }  -- ← this was the missing piece
+      { require("brain-battery").lualine_component() }
     )
     return opts
   end,
